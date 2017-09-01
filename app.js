428166e855034dfb2987c9a7ff5de7526d0a5d9d
Marto <martinkaldramov1@gmail.com>
2017-09-01 07:40:01 +0300

    adding functionality to addNote function

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
-
 const argv = yargs.argv;
+
+var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs:', argv);
 
@@ -18,7 +18,7 @@ else if(command === 'list')
   notes.getAll();
 else if(command === 'read')
   notes.readNote(argv.title);
-else if(command === 'delete')
+else if(command === 'remove')
   notes.deleteNote(argv.title);
 else
   console.log('Command not found');
